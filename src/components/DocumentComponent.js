4324c5032a31d97183366479dc0f21763b91d2fa
napalskies <napalskies@gmail.com>
2025-11-18 17:07:30 +0200

    add implementation for list of active users

--- src/components/DocumentComponent.js
+++ src/components/DocumentComponent.js
@@ -1,13 +1,14 @@
 import React, { useEffect, useRef, useState, useContext } from 'react';
 import axios from 'axios';
 
-import { debounce } from 'lodash';
+import { debounce, forEach } from 'lodash';
 import { SignalRContext } from '../providers/SignalRProvider';
 
 function DocumentComponent(props) {
     const {connectionContext, connectionReady} = useContext(SignalRContext);
     const documentId = props.documentId;
     const [inputText, setInputText] = useState('');
+    const [activeUsers, setActiveUsers] = useState([]);
 
     useEffect(() => {
         async function initInput() {
@@ -27,7 +28,7 @@ function DocumentComponent(props) {
                 console.log(connectionContext.state);
                 await connectionContext.invoke("JoinDocument", documentId);
             }
-            console.log(connectionReady, connectionContext.state);
+            console.log("New user has joined document.");
         };
 
         joinWhenConnected().catch(err => console.error("Error in invoking JoinDocument:", err));
@@ -60,32 +61,84 @@ function DocumentComponent(props) {
       catch (err) {
         console.error("Invoke failed: ", err);
       }
-    }, 200),
-  []);
+    }, 200));
 
 
 
   useEffect(() => {
+    console.log(documentId + " mounted");
     const debouncedFn = handleDocumentChanged.current;
     const onDocumentChanged = (change) => {console.log(change); setInputText(change)};
-    const onUserConnected = (connectionId) => console.log(`User ${connectionId} connected!`);
-    const onUserDisconnected = (connectionId) => console.log(`User ${connectionId} disconnected!`);
-    connectionContext.on("DocumentChanged", onDocumentChanged);
-    connectionContext.on("UserConnected", onUserConnected);
-    connectionContext.on("UserDisconnected", onUserDisconnected);
+
+    connectionContext.on("DocumentChanged"+documentId, onDocumentChanged);
 
     return () => {
       console.log("Cleaning up handlers");   
        
       debouncedFn.cancel();
-       connectionContext.off("DocumentChanged", onDocumentChanged);//, onDocumentChanged);
-       connectionContext.off("UserConnected", onUserConnected);
-       connectionContext.off("UserDisconnected", onUserDisconnected);
+       connectionContext.off("DocumentChanged"+documentId, onDocumentChanged);//, onDocumentChanged);
+       console.log(documentId + " unmounted");
+    };
+  }, [connectionContext, documentId]);
+
+  useEffect(() => {
+    const onUserConnected = (connectionId) => 
+    {
+        // console.log(activeUsers);
+        // setActiveUsers( [...activeUsers, connectionId] );
+         console.log(`User ${connectionId} connected!`);
+        // console.log(activeUsers);
+    }
+    const onUserDisconnected = (connectionId) => 
+    {
+        console.log(`User ${connectionId} disconnected!`);
+        // console.log(activeUsers);
+        // if(activeUsers.find(connectionId))
+        //     setActiveUsers(activeUsers.filter(user => user !== connectionId));
+    }
+
+    connectionContext.on("UserConnected", onUserConnected);
+    connectionContext.on("UserDisconnected", onUserDisconnected);
+
+    return () => {
+        connectionContext.off("UserConnected", onUserConnected);
+        connectionContext.off("UserDisconnected", onUserDisconnected);
     };
   }, [connectionContext]);
 
+  useEffect(() => {
+    console.log("Active users updated: ", activeUsers);
+    const onActiveUsersUpdated = (users) => {
+        console.log("Received active users: ", users);
+        setActiveUsers(users);
+    }
+    connectionContext.on("ActiveUsersUpdated"+documentId, onActiveUsersUpdated);
+
+    return () => {
+        connectionContext.off("ActiveUsersUpdated"+documentId, onActiveUsersUpdated);
+    };
+  }, [activeUsers, documentId, connectionContext]);
+
+  useEffect(() => {
+    return () => {
+        console.log(connectionContext.state)
+        if(connectionContext.state === "Connected"){
+        console.log("Goodbye im unmounting" + documentId);
+        const onLeaveDocument = async () => {
+            await connectionContext.invoke("LeaveDocument", documentId);
+            console.log("Just invoked LeaveDocument");
+        }
+        onLeaveDocument().catch(err => console.error("Error in invoking LeaveDocument:", err));}
+    };
+  }, [connectionContext, documentId]);
+
   return (
-    <input value = {inputText} onChange={(e) => {setInputText(e.target.value); handleDocumentChanged.current(e.target.value)}}></input>
+    <>
+        <input value = {inputText} onChange={(e) => {setInputText(e.target.value); handleDocumentChanged.current(e.target.value)}}></input>
+        <div className = "active-users-footer">
+            {activeUsers && activeUsers.length > 0 && activeUsers.map(user => <p key={user}>{user}</p>)}
+        </div>
+    </>
   );
 }
 
